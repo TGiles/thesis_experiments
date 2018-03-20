ea6d0ca03ecca1ae6559e29789bf3837c87ac0cb
Tim Giles <timothy.giles.12@cnu.edu>
2018-03-20 19:14:45 -0400

    successful run

--- scripts/plan_parser.py
+++ scripts/plan_parser.py
@@ -1,29 +1,60 @@
 import rospy, math
 from std_msgs.msg import String
 from vigir_footstep_planning_msgs.msg import StepPlan, Step, Foot, StepPlanRequestActionResult, StepPlanRequestActionGoal
+from plan_publisher import *
+from plan_writer import *
 # from vigir_footstep_planning_msgs.action import StepPlanRequestActionGoal
 
+def set_callback_done():
+    global callback_done
+    callback_done = not callback_done
 
-callback_done = False
+
+def get_file_writer():
+    global file_writer
+    return file_writer
+
+
+def calc_avgs(result_list):
+    length = len(result_list)
+    avg_body_centroid = 0.0
+    avg_steps = 0
+    avg_final_eps = 0.0
+    avg_exp_states = 0
+    avg_plan_time = 0.0
+    avg_path_cost = 0.0
+    for thing in result_list:
+        avg_body_centroid += thing[0]
+        avg_steps += thing[1]
+        avg_final_eps += thing[2]
+        avg_exp_states += thing[3]
+        avg_plan_time += thing[4]
+        avg_path_cost += thing[5]
+    avg_body_centroid = avg_body_centroid / length
+    avg_steps = avg_steps / length
+    avg_final_eps = avg_final_eps / length
+    avg_exp_states = avg_exp_states / length
+    avg_plan_time = avg_plan_time / length
+    avg_path_cost = avg_path_cost / length
+    return [avg_body_centroid, avg_steps, avg_final_eps, avg_exp_states, avg_plan_time, avg_path_cost]
 
 def callback(data):
-    # data is whole plan
-    # rospy.loginfo(data.result.step_plan)
-    uh = [data.result.step_plan.steps]
-    rospy.loginfo('Length of uh: %f', len(uh))
-    rospy.loginfo('in callback fn')
-    for thing in uh:
-        rospy.loginfo(thing)
-    return
+    global file_writer
     global callback_done
+    global run_result
+    global setup_id
+    global plugin_set_id
+    global iteration
+    file_writer = get_file_writer()
     distance_centroid_travel = 0.0
     current_centroid = (0.0, 0.0)
-    planned_steps = data.result.step_plan
+    planned_steps = data.result.step_plan.steps
     final_eps = data.result.final_eps
     planning_time = data.result.planning_time
     num_of_exp_states = data.result.number_of_states_expanded
+    total_path_cost = data.result.total_path_cost
     for x in range(0, len(planned_steps)):
-        if x == len(planned_steps):
+        if x == len(planned_steps) - 1:
             break
         left_foot = None
         right_foot = None
@@ -46,53 +77,97 @@ def callback(data):
         current_centroid = body_centroid
     
     # scale down to grid
-    distance_centroid_travel *= 0.02
-    rospy.loginfo('Body centroid euclidean distance traveled: %f', distance_centroid_travel)
+    # distance_centroid_travel *= 0.02
+    rospy.loginfo('--------------------------------------------')
+    rospy.loginfo('Body centroid euclidean distance: %f [m]', distance_centroid_travel)
     rospy.loginfo('Number of steps in plan: %i', len(planned_steps))
     rospy.loginfo('Final epsilon value: %f', final_eps)
-    rospy.loginfo('Planning time: %f seconds', planning_time)
     rospy.loginfo('Number of states expanded: %i', num_of_exp_states)
+    rospy.loginfo('Planning time: %f [seconds]', planning_time)
+    rospy.loginfo('Total path cost: %f', total_path_cost)
+    rospy.loginfo('--------------------------------------------')
+    # file_writer, config_id, iteration, body_centroid, num_steps, final_eps, expanded_states, plan_time, path_cost
+    config_string = str(setup_id + plugin_set_id)
+    write_run(file_writer, config_string, iteration, distance_centroid_travel, len(planned_steps), final_eps, num_of_exp_states, planning_time, num_of_exp_states)
     callback_done = True
+    run_result.append([distance_centroid_travel, len(planned_steps), final_eps, num_of_exp_states, planning_time, final_eps])
 
-def plan_parser():
-    # callback_done = False
+def plan_parser(setup_id, plugin_set_id):
+    # Example of setup id and plugin set
+    # ViGIR A
+    # means Faulty state generator, euclidean step cost, euclidean heuristic
+    global callback_done
+    global run_result
+    global file_writer
+    global iteration
+    # global setup_id
+    # global plugin_set_id
+    callback_done = True
+    current_test = create_test_dir()
+    current_setup = create_setup_dir(current_test, setup_id)
+    run_result = []
     rospy.init_node('plan_parser', anonymous=True)
     rospy.Subscriber('/vigir/footstep_planning/step_plan_request/result', StepPlanRequestActionResult, callback)
     rospy.loginfo('Plan_parser started up')
-    rospy.spin()
-
-
-
-def plan_publisher():
-    global callback_done
-    # rospy.init_node('plan_publisher', anonymous=True)
-    rospy.loginfo('plan_publisher started')
     pub = rospy.Publisher('/vigir/footstep_planning/step_plan_request/goal', StepPlanRequestActionGoal, queue_size=10)
+    rospy.loginfo('plan_publisher started')
+    rate = rospy.Rate(1)
+    current_config = 1
+    iteration = 1
+    file_obj = open(current_setup + '/Scenario' + str(current_config) + '.csv', 'w+')
+    file_writer = csv.writer(file_obj, delimiter=',')
+    write_header(file_writer, 'Scenario '+ str(current_config))
+    write_run_header(file_writer)
+    # plan_publisher(pub, current_config)
+    # rospy.spin()
+    while not rospy.is_shutdown():
+        rate.sleep()
+        if callback_done:
+            if current_config == 5:
+                rospy.signal_shutdown('End of testing')
+                return
+            # rospy.signal_shutdown('End of testing')
+            plan_publisher(pub, current_config, setup_id, plugin_set_id, iteration)
+            callback_done = False
+            if iteration == 5:
+                avg_results = calc_avgs(run_result)
+                write_avg_planning_task(file_writer, str(setup_id + plugin_set_id), 
+                avg_results[0], avg_results[1], avg_results[2], avg_results[3], avg_results[4], avg_results[5])
+
+                run_result = []
+                current_config += 1
+                file_obj = open(current_setup + '/Scenario' + str(current_config)+ '.csv', 'w+')
+                file_writer = csv.writer(file_obj, delimiter=',')
+                write_header(file_writer, 'Scenario' + str(current_config))
+                write_run_header(file_writer)
+                iteration = 1
+            else:
+                iteration += 1
+        else:
+            continue
+
+
+def plan_publisher(pub, config_num, setup_id=None, plugin_set_id=None, iteration=None):
+    # rospy.init_node('plan_publisher', anonymous=True)
+    global callback_done
     action_goal = StepPlanRequestActionGoal()
     rospy.loginfo('action_goal created')
-    set_action_goal(action_goal, 1)
-    rospy.loginfo('set_action_goal returned')
-    runner = True
-    current_config_number = 1
+    set_action_goal(action_goal, config_num, setup_id, plugin_set_id, iteration)
+    rospy.loginfo('set_action_goal returned: config number %i', config_num)
     pub.publish(action_goal)
-    rospy.loginfo('action goal:')
-    rospy.loginfo(action_goal)
-    rate = rospy.Rate(10) # 10hz
-    # while(runner):
-    #     rate.sleep()
-    #     rospy.loginfo('callback_done: %s', callback_done)
-    #     if not callback_done:
-    #         continue
-    #     else:
-    #         if current_config_number == 5:
-    #             runner = False
-    #             break
-    #         callback_done = False
-    #         current_config_number += 1
-    #         action_goal = set_action_goal(action_goal, current_config_number)
-    #         pub.publisher(action_goal)
-
+    callback_done = False
 
 if __name__ == '__main__':
-    plan_parser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--setup_id', type=str, default=None)
+    parser.add_argument('--plugin_set_id', type=str, default=None)
+    args = parser.parse_args()
+
+    global callback_done
+    global setup_id
+    global plugin_set_id
+    setup_id = args.setup_id
+    plugin_set_id = args.plugin_set_id
+    callback_done = True
+    plan_parser(args.setup_id, args.plugin_set_id)
     # plan_publisher()
--- scripts/plan_writer.py
+++ scripts/plan_writer.py
@@ -4,36 +4,93 @@ import csv
 import datetime
 
 def create_test_dir():
-    experiment_dir = '../experiment_data'
+    experiment_dir = 'experiment_data'
     test_dir = str(datetime.datetime.now())
     try:
         os.makedirs(experiment_dir + '/' + test_dir)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    os.chdir(experiment_dir + '/' + test_dir, 0776)
+    os.chmod(experiment_dir + '/' + test_dir, 0776)
+    return str(experiment_dir + '/' + test_dir)
+
+def create_setup_dir(current_test_dir, setup_id):
+    try:
+        os.makedirs(current_test_dir + '/' + setup_id)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    os.chmod(current_test_dir + '/' + setup_id, 0766)
+    return str(current_test_dir + '/' + setup_id)
+
 
 # Need CSV for each scenario, similar to thesis tables
 
-def write_header(file_obj, planning_scenario):
+def write_header(file_writer, planning_scenario):
     ''' should write
     Planning Scenario #
-
     '''
+    file_writer.writerow([planning_scenario])
 
-def write_run(file_obj):
+def write_run_header(file_writer):
+    file_writer.writerow([
+        'Configuration ID',
+        'Run #',
+        'Body Centroid Distance [m]',
+        '# Steps',
+        'Final Epsilon',
+        '# Expanded States',
+        'Planning Time [s]',
+        'Path Cost'
+    ])
+
+def write_run(file_writer, config_id, iteration, body_centroid, num_steps, final_eps, expanded_states, plan_time, path_cost):
     ''' should write
     Configuration ID
+    Run #
     Body Centroid Distance [m]
     # Steps
+    Final Epsilon
     # Expanded States
     Planning Time [s]
     Path Cost
     '''
+    file_writer.writerow([
+        config_id,
+        iteration,
+        body_centroid,
+        num_steps,
+        final_eps,
+        expanded_states,
+        plan_time,
+        path_cost
+    ])
 
-def write_avg_planning_task(file_obj):
+def write_avg_planning_task(file_writer, config_id, avg_body, avg_steps, avg_final_eps, avg_states_expanded, avg_plan_time, avg_path_cost):
     ''' should write
     Configuration ID
-
-
+    Average Body Centroid Distance [m]
+    Average # Steps
+    Average Final Epsilon
+    Average # Expanded States
+    Average Planning Time [s]
+    Average Path Cost
     '''
+    file_writer.writerow([
+        'Configuration ID',
+        'Average Body Centroid Distance [m]',
+        'Average # Steps',
+        'Average Final Epsilon',
+        'Average # Expanded States',
+        'Average Planning Time [s]',
+        'Average Path Cost'
+    ])
+    file_writer.writerow([
+        config_id,
+        avg_body,
+        avg_steps,
+        avg_final_eps,
+        avg_states_expanded,
+        avg_plan_time,
+        avg_path_cost
+    ])
