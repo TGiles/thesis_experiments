8b6a03f4b4fff97ddb440e812c2069ae0401e230
Tim Giles <timothy.giles.12@cnu.edu>
2018-03-21 20:15:41 -0400

    add additional normal check

--- scripts/RPY_2_quaternion.py
+++ scripts/RPY_2_quaternion.py
@@ -1,5 +1,15 @@
 import rospy
 from tf.msg import tfMessage
 from tf.transformations import quaternion_from_euler
-q = quaternion_from_euler(0,0, 3.134314)
+import numpy as np
+def normalize(array):
+    quat = np.array(array)
+    return quat / np.sqrt(np.dot(quat, quat))
+
+
+q = quaternion_from_euler(0,0, 1)
 print q
+print 'before normal'
+q = normalize(q)
+print q
+print 'after normal'
